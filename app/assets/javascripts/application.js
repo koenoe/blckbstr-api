ec9f8c66ff0a14caf6d552e3f296de3074b56bf7
Koen Romers <info@koenromers.com>
2015-05-01 00:10:20 +0200

    feat(angular): setup angular with bower and haml

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require_tree .
+//= require angular
+//= require_tree ./app/components
+//= require app/app
