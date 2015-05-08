8c202af86f9238ccbc9e4d55276d43f257a36315
Koen Romers <info@koenromers.com>
2015-05-08 19:44:36 +0200

    feat(movie): model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,17 +1,2 @@
-class Movie
-  include ActiveModel::Model
-
-  attr_accessor :title,
-                :release_date,
-                :imdb_id,
-                :backdrop_path,
-                :backdrop_base_url
-
-  def backdrop_url(size = 'original')
-    if self.backdrop_base_url.blank?
-      self.backdrop_base_url = 'http://image.tmdb.org/t/p/'
-    end
-    self.backdrop_base_url + size + self.backdrop_path
-  end
-
+class Movie < ActiveRecord::Base
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150508173733) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title",             limit: 255
+    t.string   "tmdb_id",           limit: 255
+    t.string   "imdb_id",           limit: 255
+    t.string   "letterboxd_slug",   limit: 255
+    t.string   "backdrop_url",      limit: 255
+    t.text     "plot",              limit: 65535
+    t.string   "trailer_url",       limit: 255
+    t.date     "release_date"
+    t.integer  "runtime",           limit: 4
+    t.float    "letterboxd_rating", limit: 24
+    t.float    "imdb_rating",       limit: 24
+    t.float    "tmdb_rating",       limit: 24
+    t.integer  "budget",            limit: 4
+    t.string   "language",          limit: 255
+    t.string   "country",           limit: 255
+    t.string   "certification",     limit: 255
+    t.integer  "metascore",         limit: 4
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  add_index "movies", ["budget"], name: "index_movies_on_budget", using: :btree
+  add_index "movies", ["certification"], name: "index_movies_on_certification", using: :btree
+  add_index "movies", ["country"], name: "index_movies_on_country", using: :btree
+  add_index "movies", ["imdb_id"], name: "index_movies_on_imdb_id", unique: true, using: :btree
+  add_index "movies", ["imdb_rating"], name: "index_movies_on_imdb_rating", using: :btree
+  add_index "movies", ["language"], name: "index_movies_on_language", using: :btree
+  add_index "movies", ["letterboxd_rating"], name: "index_movies_on_letterboxd_rating", using: :btree
+  add_index "movies", ["letterboxd_slug"], name: "index_movies_on_letterboxd_slug", unique: true, using: :btree
+  add_index "movies", ["metascore"], name: "index_movies_on_metascore", using: :btree
+  add_index "movies", ["release_date"], name: "index_movies_on_release_date", using: :btree
+  add_index "movies", ["runtime"], name: "index_movies_on_runtime", using: :btree
+  add_index "movies", ["tmdb_id"], name: "index_movies_on_tmdb_id", unique: true, using: :btree
+  add_index "movies", ["tmdb_rating"], name: "index_movies_on_tmdb_rating", using: :btree
 
 end
